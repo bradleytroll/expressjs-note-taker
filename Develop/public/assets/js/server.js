0c193b939b9bdebf03e3afffef3175e0314eed0b
Bradley Troll <bradleytroll@gmail.com>
2024-03-13 10:54:15 -0400

    Attempts to launch site, not working.

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,10 +20,43 @@ app.get('*', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '/db/db.json') 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data))
     })
 });
 
-app.post('./Develop/public/notes.html')
+app.post('/api/notes', (req, res) => {
+    const newNote = { ...req.body, id: uuidv4() };
+
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.json(newNote)
+        })
+        })
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.json({ message: 'Deleted note'})
+        })
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`)
+});

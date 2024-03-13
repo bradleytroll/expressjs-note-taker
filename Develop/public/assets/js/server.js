3785952b0f5f702c7f23db683e2a2456b4fb26f7
Bradley Troll <bradleytroll@gmail.com>
2024-03-13 10:21:30 -0400

    Attempts to add paths.

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json') 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
+});
+
+app.post('./Develop/public/notes.html')
